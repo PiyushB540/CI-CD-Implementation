5cc841b516de19abf3bf0c56376d2c12e4d28a26
PiyushB540 <p.panchabhai@braininventory.com>
2026-05-06 17:17:01 +0530

    Test Comment

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 
-// GET all users api
+// GET all users 
 router.get("/", async (req, res) => {
   const users = await User.find();
   res.json(users);
